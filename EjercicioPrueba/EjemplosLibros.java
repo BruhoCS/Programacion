77f5b77ff38d3c1b0258c23408e9afe22b1ce3ba
BruhoCS <117358458+BruhoCS@users.noreply.github.com>
2023-12-12 21:56:28 +0100

    1ºEjercicio con objetos

---
+++ EjercicioPrueba/EjemplosLibros.java
@@ -0,0 +1,15 @@
+public class EjemplosLibros{
+        //Nota: el uso de "this" se utiliza para hacer referencia a nuestro objeto dentro del codigo
+        
+        public static void main (String[] args) {
+                //Creo un objeto de libro
+                Libro lib1 = new Libro();
+                Libro lib2 = new Libro();
+                Libro lib3 = new Libro();
+                Libro lib4 = new Libro();
+
+                //Activar la accion que se creo en el class "Libro"
+                lib1.descatalogar("Descataloga")
+        }
+        
+}
